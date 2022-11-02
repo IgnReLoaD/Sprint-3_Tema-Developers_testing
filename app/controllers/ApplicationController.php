151aa3b7d3232ecd2889034b9101d9470fbf1ae7
IgnReLoaD <ortiz.ignasi@gmail.com>
2022-11-02 12:18:39 +0100

    tratando de montar un JSON de test

--- app/controllers/ApplicationController.php
+++ app/controllers/ApplicationController.php
@@ -4,7 +4,7 @@
  * Base controller for the application.
  * Add general things in this controller.
  */
-class ApplicationController extends Controller 
+class ApplicationController extends Controller  
 {
 	
 }
