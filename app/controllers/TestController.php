4d3e3f3be24fa06a472c62c2a3bf35ec427bb132
IgnReLoaD <ortiz.ignasi@gmail.com>
2022-11-08 11:52:12 +0100

    testing controllers

--- app/controllers/TestController.php
+++ app/controllers/TestController.php
@@ -1,9 +1,9 @@
 <?php
 
-// en teoria debe abrir el mensaje "hello from test"
+// en teoria   debe abrir el mensaje "hello from test"
 
 class TestController extends ApplicationController{
-	
+	// controller 
 	public function indexAction()
 	{
 		$this->view->message = "hello from test::index";
